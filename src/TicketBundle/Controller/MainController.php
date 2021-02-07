e89725ea903cf896c6eff10a02929d3cf80f4b52
oussema <oussema.zribi@esprit.tn>
2021-02-07 22:03:44 +0100

    template

---
+++ src/TicketBundle/Controller/MainController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace TicketBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class MainController extends Controller
+{
+}
---
+++ src/TicketBundle/Tests/Controller/MainControllerTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace TicketBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class MainControllerTest extends WebTestCase
+{
+}
